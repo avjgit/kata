c63cb7ab361a017f20684b733e5a2e855f4a6b67
AJ <avjmail@gmail.com>
2018-08-14 21:21:49 +0300

    Exercism Raindrops local function

--- exercism/csharp/raindrops/Raindrops.cs
+++ exercism/csharp/raindrops/Raindrops.cs
@@ -2,20 +2,16 @@ using System;
 
 public static class Raindrops
 {
-    private static int Number { get; set; }
-
-    private static string Check(int divisor, string word)
-        => Number % divisor == 0 ? word : String.Empty;
-
     public static string Convert(int number)
     {
-        Number = number;
-
+        string Check(int dividend, int divisor, string word)
+            => dividend % divisor == 0 ? word : String.Empty;
+            
         var txt = String.Empty;
        
-        txt += Check(3, "Pling");
-        txt += Check(5, "Plang");
-        txt += Check(7, "Plong");
+        txt += Check(number, 3, "Pling");
+        txt += Check(number, 5, "Plang");
+        txt += Check(number, 7, "Plong");
         
         return string.IsNullOrWhiteSpace(txt)
             ? number.ToString()
