23385d4de3e577fa7c4b688fbc23b2882139fc08
AJ <avjmail@gmail.com>
2018-08-17 21:27:04 +0300

    12 days

--- exercism/csharp/twelve-days/TwelveDays.cs
+++ exercism/csharp/twelve-days/TwelveDays.cs
@@ -1,116 +1,103 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class TwelveDays
 {
-    // my own little https://github.com/Humanizr/Humanizer :)
-    public class Number
+    private static string Amount(this int i)
     {
-        public int Value { get; set; }
-
-        public Number(int value)
+        switch (i)
         {
-            Value = value;
+            case 1: return "a";
+            case 2: return "two";
+            case 3: return "three";
+            case 4: return "four";
+            case 5: return "five";
+            case 6: return "six";
+            case 7: return "seven";
+            case 8: return "eight";
+            case 9: return "nine";
+            case 10: return "ten";
+            case 11: return "eleven";
+            case 12: return "twelve";
+            default: return i.ToString();
         }
-
-        public string Amount
+    }
+    
+    private static string Order(this int i)
+    {
+        switch (i)
         {
-            get
-            {
-                switch (Value)
-                {
-                    case 1: return "a";
-                    case 2: return "two";
-                    case 3: return "three";
-                    case 4: return "four";
-                    case 5: return "five";
-                    case 6: return "six";
-                    case 7: return "seven";
-                    case 8: return "eight";
-                    case 9: return "nine";
-                    case 10: return "ten";
-                    case 11: return "eleven";
-                    case 12: return "tvelve";
-                    default: return base.ToString();
-                }
-            }
-        }
-
-        public string Order
-        {
-            get
-            {
-
-                switch (Value)
-                {
-                    case 1: return "first";
-                    case 2: return "second";
-                    case 3: return "third";
-                    case 4: return "fourth";
-                    case 5: return "fivth";
-                    case 6: return "sixth";
-                    case 7: return "seventh";
-                    case 8: return "eighth";
-                    case 9: return "ninth";
-                    case 10: return "tenth";
-                    case 11: return "eleventh";
-                    case 12: return "tvelfth";
-                    default: return base.ToString();
-                }
-            }
+            case 1: return "first";
+            case 2: return "second";
+            case 3: return "third";
+            case 4: return "fourth";
+            case 5: return "fifth";
+            case 6: return "sixth";
+            case 7: return "seventh";
+            case 8: return "eighth";
+            case 9: return "ninth";
+            case 10: return "tenth";
+            case 11: return "eleventh";
+            case 12: return "twelfth";
+            default: return i.ToString();
         }
     }
 
     public class ChristmasDay
     {
-        private Number day;
-
-        private string Gift
+        private Dictionary<int, string> GiftForDay = new Dictionary<int, string>()
         {
-            get
+            {1, "Partridge in a Pear Tree"},
+            {2, "Turtle Doves"},
+            {3, "French Hens"},
+            {4, "Calling Birds"},
+            {5, "Gold Rings"},
+            {6, "Geese-a-Laying"},
+            {7, "Swans-a-Swimming"},
+            {8, "Maids-a-Milking"},
+            {9, "Ladies Dancing"},
+            {10, "Lords-a-Leaping"},
+            {11, "Pipers Piping"},
+            {12, "Drummers Drumming"}
+        };
+        private readonly int day;
+
+        public ChristmasDay(int day)
+        {
+            this.day = day;
+        }
+        
+        private string Gifts()
+        {
+            string firstDayPrefix(int _i, int _day) => _i == 1 && _day > 1 ? "and " : "";
+
+            var gifts = new List<string>();
+            for (int i = day; i >= 1; i--)
             {
-                switch (day.Value)
-                {
-                    case 01: return "Partridge in a Pear Tree";
-                    case 02: return "Turtle Doves";
-                    case 03: return "French Hens";
-                    case 04: return "Calling Birds";
-                    case 05: return "Gold Rings";
-                    case 06: return "Geese-a-Laying";
-                    case 07: return "Swans-a-Swimming";
-                    case 08: return "Maids-a-Milking";
-                    case 09: return "Ladies Dancing";
-                    case 10: return "Lords-a-Leaping";
-                    case 11: return "Pipers Piping";
-                    case 12: return "Drummers Drumming";
-                    default: return String.Empty;
-                }
+                gifts.Add($"{firstDayPrefix(i, day)}{i.Amount()} {GiftForDay[i]}");
             }
+            return String.Join(", ", gifts);
         }
-
-        public ChristmasDay(int dayNr)
-        {
-            day = new Number(dayNr);
-        }
-
-        private string DayIntro() => $"On the {day.Order} day of Christmas my true love gave to me, ";
-        private string GiftOfTheDay() => $"{day.Amount} {Gift}";
-        private string PreviousDaysGifts()
 
         public override string ToString()
         {
-            return $"On the {day.Order} day of Christmas my true love gave to me, {day.Amount} {Gift}.";
+            return $"On the {day.Order()} day of Christmas my true love gave to me, {Gifts()}.";
         }
     }
-
 
 
     public static string Recite(int verseNumber)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new ChristmasDay(verseNumber).ToString();
     }
 
     public static string Recite(int startVerse, int endVerse)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var verses = new List<string>();
+        for (int i = startVerse; i <= endVerse; i++)
+        {
+            verses.Add(Recite(i));
+        }
+        return string.Join("\n", verses);
     }
 }
