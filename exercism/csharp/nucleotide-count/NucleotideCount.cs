e646a6bbf61fff42e8a697fa5c4d4c51a840fe7a
AJ <avjmail@gmail.com>
2018-08-20 21:56:20 +0300

    exc nucleotide count

---
+++ exercism/csharp/nucleotide-count/NucleotideCount.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+
+public class NucleotideCount
+{
+    private readonly string sequence;
+
+    private const char ADENINE = 'A';
+    private const char CYTOSINE = 'C';
+    private const char GUANINE = 'G';
+    private const char THYMINE = 'T';
+
+    Dictionary<char, int> nucleotideCounts;
+    private List<char> ValidNucleotides => new List<char>
+    {
+        ADENINE, CYTOSINE, GUANINE, THYMINE
+    };
+
+    public NucleotideCount(string sequence)
+    {
+        foreach (char c in sequence)
+        {
+            if (!ValidNucleotides.Contains(c))
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        this.sequence = sequence;
+    }
+
+    public IDictionary<char, int> NucleotideCounts
+    {
+        get
+        {
+            if (nucleotideCounts == null)
+                CountNucleotides();
+            return nucleotideCounts;
+        }
+    }
+
+    private void CountNucleotides()
+    {
+        nucleotideCounts = new Dictionary<char, int>
+        {
+            [ADENINE] = 0,
+            [CYTOSINE] = 0,
+            [GUANINE] = 0,
+            [THYMINE] = 0
+        };
+
+        void count(char nucleotide) => nucleotideCounts[nucleotide] = nucleotideCounts[nucleotide] + 1;
+
+        foreach (var nucleotide in sequence)
+        {
+            switch (nucleotide)
+            {
+                case ADENINE: count(ADENINE); break;
+                case CYTOSINE: count(CYTOSINE); break;
+                case GUANINE: count(GUANINE); break;
+                case THYMINE: count(THYMINE); break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- exercism/csharp/word-count/WordCount.cs
+++ exercism/csharp/word-count/WordCount.cs
@@ -5,6 +5,8 @@ public static class WordCount
 {
     public static IDictionary<string, int> CountWords(string phrase)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        bool IsSeparator(char x) => char.IsPunctuation(x) || char.IsSeparator(x) || char.IsWhiteSpace(x);
+        string uniformlySeparatedString = phrase.Replace()
+        string [] words = phrase.Split(' ')
     }
 }
