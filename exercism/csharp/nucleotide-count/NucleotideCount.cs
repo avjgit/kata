16753573e7f6d5d1f82dffe4aab354b965087b3d
AJ <avjmail@gmail.com>
2018-08-21 21:49:21 +0300

    exc nucleotide count fix

--- exercism/csharp/nucleotide-count/NucleotideCount.cs
+++ exercism/csharp/nucleotide-count/NucleotideCount.cs
@@ -1,67 +1,18 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public class NucleotideCount
 {
-    private readonly string sequence;
-
-    private const char ADENINE = 'A';
-    private const char CYTOSINE = 'C';
-    private const char GUANINE = 'G';
-    private const char THYMINE = 'T';
-
-    Dictionary<char, int> nucleotideCounts;
-    private List<char> ValidNucleotides => new List<char>
-    {
-        ADENINE, CYTOSINE, GUANINE, THYMINE
-    };
+    private List<char> ValidNucleotides => new List<char> { 'A', 'C', 'G', 'T' };
 
     public NucleotideCount(string sequence)
     {
-        foreach (char c in sequence)
-        {
-            if (!ValidNucleotides.Contains(c))
-            {
-                throw new ArgumentException();
-            }
-        }
-
-        this.sequence = sequence;
+        if (sequence.Any(c => !ValidNucleotides.Contains(c))) throw new ArgumentException();
+        NucleotideCounts = new Dictionary<char, int>();
+        ValidNucleotides.ForEach(n => NucleotideCounts[n] = 0);
+        sequence.ToList().ForEach(n => NucleotideCounts[n]++);
     }
 
-    public IDictionary<char, int> NucleotideCounts
-    {
-        get
-        {
-            if (nucleotideCounts == null)
-                CountNucleotides();
-            return nucleotideCounts;
-        }
-    }
-
-    private void CountNucleotides()
-    {
-        nucleotideCounts = new Dictionary<char, int>
-        {
-            [ADENINE] = 0,
-            [CYTOSINE] = 0,
-            [GUANINE] = 0,
-            [THYMINE] = 0
-        };
-
-        void count(char nucleotide) => nucleotideCounts[nucleotide] = nucleotideCounts[nucleotide] + 1;
-
-        foreach (var nucleotide in sequence)
-        {
-            switch (nucleotide)
-            {
-                case ADENINE: count(ADENINE); break;
-                case CYTOSINE: count(CYTOSINE); break;
-                case GUANINE: count(GUANINE); break;
-                case THYMINE: count(THYMINE); break;
-                default:
-                    break;
-            }
-        }
-    }
+    public IDictionary<char, int> NucleotideCounts { get; set; }
 }
