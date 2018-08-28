2aee6c7040225a98e3d01ca14a783b6a2f4135b6
AJ <avjmail@gmail.com>
2018-08-28 21:21:02 +0300

    ex grade school done

--- exercism/csharp/grade-school/GradeSchool.cs
+++ exercism/csharp/grade-school/GradeSchool.cs
@@ -1,20 +1,39 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GradeSchool
 {
+    private Dictionary<int, List<string>> school;
+
+    public GradeSchool()
+    {
+        school = new Dictionary<int, List<string>>();
+    }
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (school.ContainsKey(grade))
+        {
+            school[grade].Add(student);
+        }
+        else
+        {
+            school[grade] = new List<string> { student };
+        }
     }
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return school
+            .OrderBy(s => s.Key)
+            .Select(g => g.Value)
+            .SelectMany(x => x.OrderBy(s => s));
     }
 
     public IEnumerable<string> Grade(int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return school.ContainsKey(grade)
+            ? school[grade].OrderBy(x => x).ToList()
+            : new List<string>();
     }
 }
