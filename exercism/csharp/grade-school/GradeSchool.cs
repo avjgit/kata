32025f07135e270ff613a2f868d9e0ddc43e9529
AJ <avjmail@gmail.com>
2018-08-29 21:08:11 +0300

    ex gradeschool done

--- exercism/csharp/grade-school/GradeSchool.cs
+++ exercism/csharp/grade-school/GradeSchool.cs
@@ -1,39 +1,21 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public class GradeSchool
 {
-    private Dictionary<int, List<string>> school;
+    private readonly Dictionary<int, List<string>> _school;
 
-    public GradeSchool()
-    {
-        school = new Dictionary<int, List<string>>();
-    }
+    public GradeSchool() => _school = new Dictionary<int, List<string>>();
+
     public void Add(string student, int grade)
     {
-        if (school.ContainsKey(grade))
-        {
-            school[grade].Add(student);
-        }
-        else
-        {
-            school[grade] = new List<string> { student };
-        }
+        if (!_school.ContainsKey(grade)) _school[grade] = new List<string>();
+        _school[grade].Add(student);
     }
 
-    public IEnumerable<string> Roster()
-    {
-        return school
-            .OrderBy(s => s.Key)
-            .Select(g => g.Value)
-            .SelectMany(x => x.OrderBy(s => s));
-    }
+    public IEnumerable<string> Roster() => _school.OrderBy(s => s.Key).SelectMany(x => x.Value.OrderBy(s => s));
 
-    public IEnumerable<string> Grade(int grade)
-    {
-        return school.ContainsKey(grade)
-            ? school[grade].OrderBy(x => x).ToList()
-            : new List<string>();
-    }
+    public IEnumerable<string> Grade(int grade) => _school.ContainsKey(grade)
+        ? _school[grade].OrderBy(x => x).ToList()
+        : Enumerable.Empty<string>();
 }
