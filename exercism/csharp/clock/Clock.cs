891d0840946d17b680ba9bfb8ecac76f686ff6ac
AJ <avjmail@gmail.com>
2019-01-26 22:28:00 +0200

    multiple exercises init

--- exercism/csharp/clock/Clock.cs
+++ exercism/csharp/clock/Clock.cs
@@ -2,39 +2,38 @@ using System;
 
 public class Clock
 {
+    private int MINUTES_OVERLAP = 60;
+    private int HOURS_OVERLAP = 24;
+
     public Clock(int hours, int minutes)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var minutes_total = HOURS_OVERLAP * MINUTES_OVERLAP + hours * MINUTES_OVERLAP + minutes;
+        var hour_less = minutes_total % MINUTES_OVERLAP < 0 ? -1 : 0;
+        Hours = (HOURS_OVERLAP + minutes_total / MINUTES_OVERLAP + hour_less) % HOURS_OVERLAP;
+        Minutes = (MINUTES_OVERLAP + minutes_total % MINUTES_OVERLAP) % MINUTES_OVERLAP;
     }
 
-    public int Hours
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
-
-    public int Minutes
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+    public int Hours { get;  }
+    public int Minutes { get;  }
 
     public Clock Add(int minutesToAdd)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new Clock(Hours, Minutes + minutesToAdd);
     }
 
     public Clock Subtract(int minutesToSubtract)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new Clock(Hours, Minutes + minutesToSubtract);
     }
 
     public override string ToString()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return $"{Hours.ToString("D2")}:{Minutes.ToString("D2")}";
+    }
+
+    public override bool Equals(Object o)
+    {
+        var other = o as Clock;
+        return other.Hours == Hours && other.Minutes == Minutes;
     }
 }
