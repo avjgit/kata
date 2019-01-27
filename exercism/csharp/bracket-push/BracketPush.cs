b9a8ff59b720010340823b3ae8b78b318caa9a3f
AJ <avjmail@gmail.com>
2019-01-27 06:33:45 +0200

    brackets

--- exercism/csharp/bracket-push/BracketPush.cs
+++ exercism/csharp/bracket-push/BracketPush.cs
@@ -1,9 +1,37 @@
-using System;
+using System.Collections.Generic;
 
 public static class BracketPush
 {
+    private static bool IsOpening(this char c) =>
+        new List<char> { '[', '{', '(' }.Contains(c);
+
+    private static bool IsClosing(this char c) =>
+        new List<char> { ']', '}', ')' }.Contains(c);
+
+    private static bool Matches(this char c, char x) =>
+        new Dictionary<char, char>
+        {
+            { '[', ']' },
+            { '{', '}' },
+            { '(', ')' }
+        }[x] == c;
+
     public static bool IsPaired(string input)
     {
-        throw new NotImplementedException();
+        var braces = new Stack<char>();
+        foreach (var c in input)
+        {
+            if (c.IsOpening())
+            {
+                braces.Push(c);
+            }
+            if (c.IsClosing())
+            {
+                if (braces.Count == 0) return false;
+                var previous = braces.Pop();
+                if (!c.Matches(previous)) return false;
+            }
+        }
+        return braces.Count == 0;
     }
 }
