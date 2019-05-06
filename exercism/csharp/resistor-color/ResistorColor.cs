07b4bb9f2e84cc611b97da76d41efa6f4f1b7742
AJ <avjmail@gmail.com>
2019-05-06 06:43:59 +0300

    resistorcolor

--- exercism/csharp/resistor-color/ResistorColor.cs
+++ exercism/csharp/resistor-color/ResistorColor.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class ResistorColor
 {
-    public static int ColorCode(string color)
+    private static List<string> ResistorsColors = new List<string>
     {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    };
 
-    public static string[] Colors()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static int ColorCode(string color) => ResistorsColors.IndexOf(color);
+
+    public static string[] Colors() => ResistorsColors.ToArray();
 }
