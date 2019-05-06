0e8b8978f74ca1e65a5291e9c044b706c9868654
AJ <avjmail@gmail.com>
2019-05-06 06:57:00 +0300

    series enumerable

--- exercism/csharp/series/Series.cs
+++ exercism/csharp/series/Series.cs
@@ -1,17 +1,14 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
 public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
         if (sliceLength <= 0 || sliceLength > numbers.Length) throw new ArgumentException();
-
-        var slices = new List<string>();
-        for (int i = 0; i + sliceLength <= numbers.Length; i++)
-        {
-            slices.Add(numbers.Substring(i, sliceLength));
-        }
-        return slices.ToArray();
+        return Enumerable
+            .Range(0, numbers.Length - sliceLength + 1)
+            .Select(x => numbers.Substring(x, sliceLength))
+            .ToArray();
     }
 }
