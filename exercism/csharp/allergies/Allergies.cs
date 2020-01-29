b003e2f83ce300a2960425077d196d63144228d4
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2020-01-29 22:00:15 +0200

    done

--- exercism/csharp/allergies/Allergies.cs
+++ exercism/csharp/allergies/Allergies.cs
@@ -1,30 +1,39 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
+[Flags]
 public enum Allergen
 {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate= 32,
+    Pollen= 64,
+    Cats = 128
 }
 
 public class Allergies
 {
-    public Allergies(int mask)
-    {
-    }
+    int allergiesMask;
+ 
+    public Allergies(int mask) => allergiesMask = mask;
 
-    public bool IsAllergicTo(Allergen allergen)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public bool IsAllergicTo(Allergen allergen) => ((int)allergen & allergiesMask) > 0;
 
     public Allergen[] List()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var allergies = new List<Allergen>();
+        foreach (Allergen a in (Allergen[])Enum.GetValues(typeof(Allergen)))
+            if(IsAllergicTo(a)) allergies.Add(a);
+        return allergies.ToArray();
+
+        //IEnumerable<int> x = Enum.GetValues(typeof(Allergen));
+        //x.Where(z => z.is)
+
+        //.ToList<Allergen>().Where(IsAllergicTo).ToArray()
     }
 }
