4a3bf0e213e376cfe91f5276cdb5a801403c19ed
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2020-02-01 15:41:56 +0200

    resistor-color fix

--- exercism/javascript/resistor-color.js
+++ exercism/javascript/resistor-color.js
@@ -1,9 +1,4 @@
-export const colorCode = (color) => {
-  for(var i = 0; i < COLORS.length; i++)
-  {
-    if (COLORS[i] === color) return i;
-  }
-};
+export const colorCode = (color) => COLORS.indexOf(color);
 
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
