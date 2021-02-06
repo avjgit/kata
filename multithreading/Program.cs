93af16dc8a002d3c7e24d2aa0553e28f6cf350ca
AJ <avjmail@gmail.com>
2021-02-06 21:14:52 +0200

    multithreading init

---
+++ multithreading/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let' s check thread:");
+            var t = Thread.CurrentThread;
+            t.Name = "The Thread";
+            Console.WriteLine(t.Name);
+        }
+    }
+}
