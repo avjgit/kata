9f434c0637372b70082c91de4de4375b47f76da8
AJ <avjmail@gmail.com>
2021-02-06 23:57:41 +0200

    multithreading some chaotic race

--- multithreading/Program.cs
+++ multithreading/Program.cs
@@ -3,16 +3,37 @@ using System.Threading;
 
 namespace multithreading
 {
+    class SomeHeavyCalculator
+    {
+        public void Calculate()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(new Random().Next(5));
+                Console.WriteLine($"Performing calculation step {i}");
+            }
+        }
+    }
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            var t = new Thread(SomeFunction);
-            t.Name = "foreground thread";
-            t.IsBackground = false;
-            t.Start();
+            var calc = new SomeHeavyCalculator();
+            var threads = new Thread[5];
 
-            Console.WriteLine("Main thread running");
+            for (int i = 0; i < 5; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(calc.Calculate));
+                threads[i].Name = $"thread #{i}";
+            }
+
+            foreach(var t in threads)
+            {
+                t.Start();
+            }
+
         }
 
         static void SomeFunction()
