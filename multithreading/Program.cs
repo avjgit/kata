1511b9ad2a8550134e1c14f71640816de95be4f8
AJ <avjmail@gmail.com>
2021-02-07 10:27:48 +0200

    multithreading some chaotic race cleanup

--- multithreading/Program.cs
+++ multithreading/Program.cs
@@ -3,44 +3,13 @@ using System.Threading;
 
 namespace multithreading
 {
-    class SomeHeavyCalculator
-    {
-        public void Calculate()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                Thread.Sleep(new Random().Next(5));
-                Console.WriteLine($"Performing calculation step {i}");
-            }
-        }
-    }
-
     class Program
     {
 
         static void Main(string[] args)
         {
-            var calc = new SomeHeavyCalculator();
-            var threads = new Thread[5];
-
-            for (int i = 0; i < 5; i++)
-            {
-                threads[i] = new Thread(new ThreadStart(calc.Calculate));
-                threads[i].Name = $"thread #{i}";
-            }
-
-            foreach(var t in threads)
-            {
-                t.Start();
-            }
-
+            RaceConditionExample.Run(); // some chaotic unordered execution
         }
 
-        static void SomeFunction()
-        {
-            Console.WriteLine($"{Thread.CurrentThread.Name} started") ;
-            Thread.Sleep(3000);
-            Console.WriteLine($"{Thread.CurrentThread.Name} finished") ;
-        }
     }
 }
---
+++ multithreading/RaceConditionExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace multithreading
+{
+    public class RaceConditionExample
+    {
+        static public void Run()
+        {
+            var calc = new SomeHeavyCalculator();
+            var threads = new Thread[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(calc.Calculate));
+                threads[i].Name = $"thread #{i}";
+            }
+
+            foreach (var t in threads)
+            {
+                t.Start();
+            }
+        }
+
+        class SomeHeavyCalculator
+        {
+            public void Calculate()
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(new Random().Next(5));
+                    Console.WriteLine($"Performing calculation step {i}");
+                }
+            }
+        }
+
+    }
+}
