ca24994c84206d862c563e591705ac8bee62fb79
AJ <avjmail@gmail.com>
2021-02-07 16:32:18 +0200

    multithreading mutex

---
+++ multithreading/MutexDemo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace multithreading
+{
+    public class MutexDemo
+    {
+        private static readonly Mutex mutex = new Mutex();
+
+        public static void Run()
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                var t = new Thread(new ThreadStart(Calculate));
+                t.Name = $"thread #{i}";
+                t.Start();
+            }
+        }
+
+        public static void Calculate()
+        {
+            try
+            {
+                mutex.WaitOne();
+                Console.WriteLine($"{Thread.CurrentThread.Name} started");
+                Thread.Sleep(1000);
+                Console.WriteLine($"{Thread.CurrentThread.Name} finished");
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
--- multithreading/Program.cs
+++ multithreading/Program.cs
@@ -5,13 +5,12 @@ namespace multithreading
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            RaceDemo.RunRaced(); // some chaotic unordered execution
-            RaceDemo.RunLocked(); // some chaotic unordered execution
-            //DeadlockExample.Run();
+            //RaceDemo.RunRaced(); // some chaotic unordered execution
+            //RaceDemo.RunLocked(); 
+            //DeadlockDemo.Run();
+            MutexDemo.Run();
         }
-
     }
 }
